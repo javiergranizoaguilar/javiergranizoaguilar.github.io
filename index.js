89656bc461ce044f946d7b2bd056fc7cb31c3941
javier <javiercelia123@gmail.com>
2023-05-26 17:53:21 +0200

    new file:   P4id.gif 	modified:   index.css 	modified:   index.html 	modified:   index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const menuIcon = document.querySelector('.menu-icon');
 const menu = document.querySelector('.menu');
 
@@ -6,5 +7,15 @@ menuIcon.addEventListener('click', () => {
 });
 document.getElementById("cambiarFondoBtn").addEventListener("click", function() {
     document.body.classList.toggle("cambiarFondo");
+  });  
+  document.getElementById("cambiarFondoBtn").addEventListener("click", function() {
+    var imagenes = document.getElementsByTagName("img");
+  for (var i = 0; i < imagenes.length; i++) {
+    if (imagenes[i].src.endsWith("download5.gif")) {
+      imagenes[i].src = "P4id.gif";
+    } else if (imagenes[i].src.endsWith("P4id.gif")) {
+      imagenes[i].src = "download5.gif";
+    }
+  }
   });
   
