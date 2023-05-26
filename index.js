6f0ad9c80ac6d19f6afc99c9505eb1c4bc58a322
javier <javiercelia123@gmail.com>
2023-05-26 15:49:52 +0200

    mejora de contenidos

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ const menu = document.querySelector('.menu');
 menuIcon.addEventListener('click', () => {
   menu.classList.toggle('active');
 });
+document.getElementById("cambiarFondoBtn").addEventListener("click", function() {
+    document.body.classList.toggle("cambiarFondo");
+  });
+  
